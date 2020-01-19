f38143b811f609d13fdb61d6af5ab51d126ca91d
Arsène Lapostolet <arsene@lapostolet.fr>
2020-01-19 21:47:21 +0100

    update new concerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,6 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material.module';
 import { AccueilComponent } from './accueil/accueil.component';
-import {AngularFireModule} from '@angular/fire';
-import {environment} from '../environments/environment';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
@@ -21,8 +18,6 @@ import {FlexLayoutModule} from '@angular/flex-layout';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     FlexLayoutModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyD_w87TOyj4R9kSLUgswQgptmK_9CK11Ak',
-    authDomain: 'rencontres-contemporaines.firebaseapp.com',
-    databaseURL: 'https://rencontres-contemporaines.firebaseio.com',
-    projectId: 'rencontres-contemporaines',
-    storageBucket: '',
-    messagingSenderId: '687400185276',
-    appId: '1:687400185276:web:d86fcafc31753115'
-  }
 };
 
 /*
